cd62daf5627d2e40faa25a8c2563f9a0e7fa2ddd
Tom Gibara <me@tomgibara.com>
2015-05-27 18:12:35 +0100

    Whitespace.

--- src/test/java/com/tomgibara/geo/EllipsoidTest.java
+++ src/test/java/com/tomgibara/geo/EllipsoidTest.java
@@ -8,5 +8,5 @@ public class EllipsoidTest extends TestCase {
 		assertTrue(Math.abs(Ellipsoid.WGS84.b - 6356752.3142) <= 0.00005);
 		assertTrue(Math.abs(Ellipsoid.GRS80.b - 6356752.3141) <= 0.00005);
 	}
-	
+
 }
